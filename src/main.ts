6f9712176610b980fc7f56063f8f37ea17b42d44
pariwesh <pariweshtamrakar@gmail.com>
2025-12-24 09:45:48 +1100

    modified cors

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:5173',
+      'http://localhost:3000',
       'https://domain-ranking-frontend.vercel.app',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
